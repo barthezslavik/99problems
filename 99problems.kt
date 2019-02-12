b0e7874b794884d4a8001b1d7d9367d409d41a22
Loginov Slava <barthez.slavik@gmail.com>
2019-02-12 14:16:53 +0200

    (02) Last but one

--- 99problems.kt
+++ 99problems.kt
@@ -1,6 +1,11 @@
 package learning 
  
 fun main(args : Array<String>) { 
-  val last = listOf(1, 2, 3, 4, 5).last()
-  println(last) 
+  val list = listOf(1, 2, 3, 4, 5)
+  val last = list.last()
+  println("(01) $last")
+
+  val size = list.size
+  val last_but_one = list.get(size-2)
+  println("(02) $last_but_one")
 }
