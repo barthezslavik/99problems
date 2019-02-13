aa7df6e1ae363a95fe9f930771e8ad390c165d02
Loginov Slava <barthez.slavik@gmail.com>
2019-02-13 10:16:46 +0200

    04, 05, 06, 07

--- 99problems.kt
+++ 99problems.kt
@@ -6,13 +6,36 @@ fun main(args : Array<String>) {
   println("(01) $last")
 
   val size = list.size
-  val last_but_one = list.get(size-2)
-  println("(02) $last_but_one")
+  val lastButOne = list[size-2]
+  println("(02) $lastButOne")
   
-  val result = nth(2, list)
-  println("(03) $result")
+  val nth = nth(2, list)
+  println("(03) $nth")
+
+  println("(04) $size")
+
+  val rev = list.asReversed()
+  println("(05) $rev")
+
+  val plist = listOf(1, 2, 3, 2, 1)
+  val pal = isPalindrome(plist)
+  println("(06) $pal")
+
+  val nested = listOf(1, listOf(5), listOf(1,3))
+  val flat = mutableListOf<Any?>()
+  flattenList(nested, flat)
+  println("(07) $flat")
 }
 
-fun nth(n: Int, list: List<Int>):Int {
-  return list.get(n+1)
+fun nth(n: Int, list: List<Int>) = list[n+1]
+
+fun isPalindrome(list: List<Int>) = list == list.asReversed()
+
+fun flattenList(nestedList: List<Any?>, flatList: MutableList<Any?>) {
+  nestedList.forEach{
+    e -> when(e){
+      !is List<Any?> -> flatList.add(e)
+      else -> flattenList(e, flatList)
+    }
+  }
 }
